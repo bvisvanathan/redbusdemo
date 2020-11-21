6addbbb97a15401877d3644afa9e7293db0aa368
bvisvanathan <balaji.visvanathan@gmail.com>
2020-11-21 17:08:08 +0000

    PAGE OBJECT AND EXCEL DTAA TEST DATA REDBUS

---
+++ src/com/redbus/LoginTest.java
@@ -0,0 +1,48 @@
+package com.redbus;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.pages.LoginPage;
+
+public class LoginTest extends BaseClass {
+	    
+	    
+	XSSFWorkbook Workbook;
+    XSSFSheet sheet1;
+    
+    @BeforeTest
+    public void fetchdata() throws IOException {
+        
+        System.out.println("WORKS");
+    	FileInputStream fis = new FileInputStream("exceldata.xlsx");
+    	System.out.println("WORKS READ");
+        Workbook = new XSSFWorkbook(fis);
+        System.out.println("WORKS OPEN");
+        sheet1 = Workbook.getSheetAt(0);  
+        System.out.println("WORKS GOT");
+        
+        
+    }       
+	        
+	@Test
+	public void login() {
+	       
+		System.out.println("WORKS METHOD");
+	   LoginPage loginpage = new LoginPage(driver);
+	   String MobileNo =  sheet1.getRow(0).getCell(0).getStringCellValue();
+	   loginpage.LogintoApp(MobileNo);
+	        
+	        
+	    }
+
+	    
+	    
+	}
+
+
