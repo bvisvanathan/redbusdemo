a408f2fdb25eb94f07b932b5487231338b2e7cda
bvisvanathan <balaji.visvanathan@gmail.com>
2020-11-28 16:08:42 +0000

    pushign code for github for jenkins

--- src/com/redbus/ListnerTestng.java
+++ src/com/redbus/ListnerTestng.java
@@ -2,6 +2,7 @@ package com.redbus;
 
 import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestResult;
 
 public class ListnerTestng implements ITestListener {
 	
@@ -9,5 +10,41 @@ public class ListnerTestng implements ITestListener {
         System.out.println(Result.getName()+"case finished");    
         
     }
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
